9deb177d39f7bc6a5600e4dabab673720a7afc99
Ad <aniketpatil354@gmail.com>
2021-09-14 16:02:01 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+
+    cout<<"Enter two numbers to be added"<<endl;
+    cin>>a;
+    cin>>b;
+    c=a+b;
+    cout<<"The sum is "<<c<<endl;
+    return 0;
+}
