40338efaed80867545232d4fd8192c4dcaaeb57b
Ad <aniketpatil354@gmail.com>
2021-09-14 16:22:08 +0530

    added multiply.cpp from ad/multiply

---
+++ multiply.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+
+    cout<<"Enter two numbers to be multiplied "<<endl;
+    cin>>a;
+    cin>>b;
+    c=a*b;
+    cout<<"The ans is "<<c<<endl;
+    return 0;
+}
